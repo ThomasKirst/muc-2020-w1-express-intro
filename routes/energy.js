67339008456b5ee7016d78aa0dc9dc4502ddf812
Thomas Kirst <thomas.kirst@neuefische.de>
2020-11-10 15:48:04 +0100

    Added energy GET route

--- routes/energy.js
+++ routes/energy.js
@@ -5,6 +5,17 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
+router.get('/:studentId', (req, res) => {
+  const { studentId } = req.params;
+  const yourLevel = getStudentEnergyByStudentId(studentId);
+  const studentsAverage = getAverageEnergyLevel();
+
+  res.json({
+    yourEnergyLevel: yourLevel,
+    yourFellowStudentsAverage: studentsAverage,
+  });
+});
+
 router.post('/', (req, res) => {
   const date = new Date();
   const timestamp = date.toUTCString();
@@ -12,5 +23,18 @@ router.post('/', (req, res) => {
   db.energy.push(newEntry);
   saveToDatabase((error) => res.json(error ?? newEntry));
 });
+
+function getAverageEnergyLevel() {
+  const sumOfEnergy = db.energy.reduce((acc, entry) => acc + entry.energy, 0);
+  const average = sumOfEnergy / db.energy.length;
+  return Math.round(average);
+}
+
+function getStudentEnergyByStudentId(studentId) {
+  const studentEnergyLevels = db.energy.find(
+    (energy) => energy.student === studentId
+  );
+  return studentEnergyLevels.energy;
+}
 
 export default router;
